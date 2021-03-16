47a6062a9cd5ea0241a54e8bcc65cb177477ccf2
JLThermos <79390068+JLThermos@users.noreply.github.com>
2021-03-15 20:29:52 -0400

    assignment 4

---
+++ Assignments/assignment 4/src/base/Main.java
@@ -0,0 +1,19 @@
+package base;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int[] array = {1,2,3,4,5,6,7,8,9,10};
+	System.out.println(sum(array));
+    }
+    static int sum(int[] array){
+        int total = 0;
+        for (int number: array) {
+            total += number;
+
+        }
+
+        return total;
+    }
+
+}
